945f3f7d47fc4553382815f47c47007e4f8f3d89
abdullaharik <mail@abdullaharik.com>
2021-12-16 16:35:35 +0300

    packages updated and styled-components removed

--- src/old.js
+++ src/old.js
@@ -1,13 +1,57 @@
 import React from 'react'
+import styled from 'styled-components'
 import Geosuggest from 'react-geosuggest'
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
+const Container = styled.div`
+  .geosuggest__suggests--hidden {
+    max-height: 0;
+    overflow: hidden;
+    border-width: 0;
+    display: none;
+  }
+  .geosuggest__item--active {
+    background: #267dc0;
+    color: #fff;
+  }
+
+  .geosuggest__input-wrapper input {
+    width: 100%;
+    font-size: 15px;
+    line-height: 1.5;
+    margin-bottom: 10px;
+  }
+  .geosuggest__suggests {
+    list-style-type: none;
+  }
+
+  .geosuggest__item {
+    cursor: pointer;
+    border-top: solid 1px rgb(223, 223, 227);
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+`
+
+const Hr = styled.div`
+  width: 100%;
+  height: 2px;
+  background-color: #ddd;
+  position: absolute;
+  left: 0;
+  margin-bottom: 10px;
+`
+
+const MapContainer = styled.div`
+  height: 400px;
+  margin-top: 10px;
+`
 
 export default class Control extends React.PureComponent {
   constructor(props) {
     super(props)
     this.state = {
-      defaultCenter: { lat: 37.857913, lng: 27.261015 },
+      defaultCenter: { lat: -34.397, lng: 150.644 },
       selected: {
         latitude: null,
         longitude: null
@@ -70,6 +114,7 @@ export default class Control extends React.PureComponent {
         onFocus={setActiveStyle}
         onBlur={setInactiveStyle}
       >
+        <Container>
           <Geosuggest
             placeholder="Start typing!"
             location={new google.maps.LatLng(53.558572, 9.9278215)}
@@ -77,8 +122,10 @@ export default class Control extends React.PureComponent {
             radius={20}
           />
 
+          <Hr />
+
           <GoogleMaps
-            containerElement={<div style={{ height: `400px` }} />}
+            containerElement={<MapContainer />}
             mapElement={<div style={{ height: `100%` }} />}
             defaultCenter={
               this.state.defaultCenter
@@ -88,8 +135,8 @@ export default class Control extends React.PureComponent {
             lat={this.state.selected.latitude}
             lng={this.state.selected.longitude}
             onClick={this.onSelectMarker}
-            draggable={true}
           />
+        </Container>
       </div>
     )
   }
@@ -97,7 +144,7 @@ export default class Control extends React.PureComponent {
 
 const GoogleMaps = withGoogleMap((props) => (
   <GoogleMap
-    defaultZoom={14}
+    defaultZoom={8}
     defaultCenter={props.defaultCenter}
     center={
       (props.lat && props.lng && { lat: props.lat, lng: props.lng }) ||
@@ -105,6 +152,7 @@ const GoogleMaps = withGoogleMap((props) => (
     }
     onClick={props.onClick}
   >
-    {props.lat && props.lng && <Marker position={{ lat: props.lat, lng: props.lng }} />}
+    {props.lat &&
+      props.lng && <Marker position={{ lat: props.lat, lng: props.lng }} />}
   </GoogleMap>
 ))
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './example/src/index.js',
@@ -19,6 +20,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new Dotenv(),
     new HtmlWebpackPlugin({
       template: './example/index.html',
       key: process.env.API_KEY
